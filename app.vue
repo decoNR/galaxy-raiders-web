5e5530d96ea73c4960acc99740244eba57bc5132
Kaiqoliver <kakadeoliveira@outlook.com>
2023-07-19 20:32:36 -0300

    Tá aparecendo o os botões e o leaderboard

--- app.vue
+++ app.vue
@@ -1,6 +1,35 @@
 <template>
-  <NuxtPage />
+  <div>
+    <nav>
+      <ul>
+        <li :class="{ active: isPageActive('/') }">
+          <nuxt-link to="/">Iniciar partida</nuxt-link>
+        </li>
+        <li :class="{ active: isPageActive('/leaderboard') }">
+          <nuxt-link to="/leaderboard">Placar</nuxt-link>
+        </li>
+        <li>
+          <a href="#" @click="exitGame">Sair</a>
+        </li>
+      </ul>
+    </nav>
+    <NuxtPage />
+  </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    isPageActive(route) {
+      return this.$route.path === route;
+    },
+    exitGame() {
+      // Implementar o código para fechar o jogo, se necessário
+      // Por exemplo: window.close();
+    }
+  }
+};
+</script>
 
 <style>
 * {
@@ -15,5 +44,28 @@ html {
 body {
   height: 100vh;
   width: 100vw;
+}
+
+nav {
+  background-color: #333;
+  padding: 10px;
+}
+
+ul {
+  list-style: none;
+  display: flex;
+}
+
+li {
+  margin-right: 20px;
+}
+
+li.active {
+  font-weight: bold;
+}
+
+a {
+  text-decoration: none;
+  color: white;
 }
 </style>
